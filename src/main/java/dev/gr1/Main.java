075c9513d066da91da4738f2f734658ec3f2a1f3
v22 <v22julian@gmail.com>
2026-05-19 13:32:21 +0200

    mvn assembly

--- src/main/java/dev/gr1/Main.java
+++ src/main/java/dev/gr1/Main.java
@@ -7,7 +7,7 @@ import spark.Spark;
 public class Main {
     public static void main(String[] args) {
         ArgParser argParser = new ArgParser();
-        argParser.addArg(ArgParser.ArgFlavor.Value, "port", 4000);
+        argParser.addArg(ArgParser.ArgFlavor.Value, "port", 4100);
 
         Args parsedArgs = argParser.parse(args);
 
@@ -15,9 +15,17 @@ public class Main {
 
         Spark.port(port);
 
-        Spark.get("/", (request, response) -> {
-            return "Hello World!";
+        Spark.before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.header("Access-Control-Allow-Methods", "*");
+            response.header("Access-Control-Allow-Headers", "*");
         });
+
+        Spark.get("/cost/test", (request, response) -> {
+            return "{cost:100,currency:\"eur\"}";
+        });
+
+        Spark.init();
 
         System.out.printf("Server started on port %d.\n", port);
     }
